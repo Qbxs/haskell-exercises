2f2c056f19c065dff6c0f994cbff479e2f89d717
Pascal Engel <ip4ssi@gmail.com>
2022-10-01 21:23:45 +0200

    add flexible instance solution

--- 02-FlexibleInstances/src/Exercises.hs
+++ 02-FlexibleInstances/src/Exercises.hs
@@ -6,29 +6,48 @@ class PopQuiz a
 -- :D This is a tricky one, but look out for nested concrete types!
 
 -- instance PopQuiz Bool
+-- no
 -- instance PopQuiz [Bool]
+-- yes
 -- instance PopQuiz [a]
+-- no
 -- instance PopQuiz (a, b)
+-- no
 -- instance PopQuiz [(a, b)]
+-- yes
 -- instance PopQuiz (IO a)
+-- no
 
 newtype RIO  r a = RIO (r -> IO a) -- Remember, this is a /new type/.
 type    RIO' r a =      r -> IO a
 
 -- instance PopQuiz (RIO Int a)
+-- yes
 -- instance PopQuiz (RIO r a)
+-- no
 -- instance PopQuiz (RIO' r a)
+-- yes
 -- instance PopQuiz (r -> IO a)
+-- yes
 -- instance PopQuiz (a -> b) -- We can write (a -> b) as ((->) a b).
+-- no
 -- instance PopQuiz (a -> b -> c)
+-- yes
 -- instance PopQuiz (a, b, c)
+-- no
 -- instance PopQuiz (a, (b, c))
+-- yes
 -- instance PopQuiz ()
+-- no
 -- instance PopQuiz (a, b, c, a)
+-- yes
 
 data Pair  a = Pair  a  a
 type Pair' a =      (a, a)
 
 -- instance PopQuiz (a, a)
+-- yes
 -- instance PopQuiz (Pair a)
+-- no
 -- instance PopQuiz (Pair' a)
+-- yes
